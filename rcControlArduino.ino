303c33461b16ab6d7885bb6b41039185f6b83696
basantashah <bindassbasanta@hotmail.com>
2019-02-03 15:10:02 +0545

    initializing all the motor driver pins

---
+++ rcControlArduino.ino
@@ -0,0 +1,23 @@
+// Define the enable pin number for PWM
+int ENALeftFirst = 5;
+int ENALeftSecond = 6;
+int ENALeftThird = 7;
+int ENALeftFourth = 8;
+int ENARightFirst = 9;
+int ENARightSecond = 10;
+int ENARightThird = 11;
+int ENARightFourth = 12;
+
+// define input pin for left side motors
+
+int INPUTLEFT1 = 13;
+int INPUTLEFT2 = 14;
+int INPUTLEFT3 = 15;
+int INPUTLEFT4 = 16;
+
+// define input pin for right side motors
+
+int INPUTRight1 = 17;
+int INPUTRight2 = 18;
+int INPUTRight3 = 19;
+int INPUTRight4 = 20;
